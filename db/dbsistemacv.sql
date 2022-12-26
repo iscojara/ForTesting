584701650fd99c49730b3152773326cee9dd2acd
PatrickMansilla <68069335+PatrickMansilla@users.noreply.github.com>
2022-12-26 15:36:48 -0500

    Add files via upload

---
+++ db/dbsistemacv.sql
@@ -0,0 +1,526 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-12-2022 a las 21:21:47
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dbsistemacv`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `articulo`
+--
+
+CREATE TABLE `articulo` (
+  `idarticulo` int(11) NOT NULL,
+  `idcategoria` int(11) NOT NULL,
+  `codigo` varchar(50) DEFAULT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `descripcion` varchar(256) DEFAULT NULL,
+  `presentacion` varchar(259) NOT NULL,
+  `fecha_vencimiento` date NOT NULL,
+  `imagen` varchar(50) DEFAULT NULL,
+  `condicion` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `articulo`
+--
+
+INSERT INTO `articulo` (`idarticulo`, `idcategoria`, `codigo`, `nombre`, `stock`, `descripcion`, `presentacion`, `fecha_vencimiento`, `imagen`, `condicion`) VALUES
+(6, 21, 'tre6', 'ACIDO TRANEXÁMICO ', 150, '20mlg', '2mL', '2023-08-31', '1658631245.jpg', 1),
+(7, 16, '12343', 'ADENOSINA ', 120, '3mg/mL', '5mL ', '2022-07-24', '1658631568.jpg', 1),
+(8, 18, '54353', 'AGUA PARA INYECCIÓN ', 130, '20mlg', '5mL ', '2023-07-31', '1658631656.jpg', 1),
+(9, 16, '936173648', 'ALTEPLASA', 140, 'SOmg', '', '2022-07-24', '1658631692.png', 1),
+(10, 19, '4324', 'AMFOTERICINA B (COMO DEOXICOLATO SÓDICO)', 135, '50mg', '5mL ', '2022-07-24', '1658631725.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idcategoria` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(256) DEFAULT NULL,
+  `condicion` tinyint(4) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idcategoria`, `nombre`, `descripcion`, `condicion`) VALUES
+(14, 'Analgésicos', 'En este grupo de tipos de medicamentos se encuentran todos los fármacos que tienen como finalidad aliviar el dolor físico, ya sea de cabeza, de articulaciones o cualquiera', 1),
+(15, 'Antiácidos y antiulcerosos', 'Dos grupos distintos de tipos de medicamentos, pero que comparten funciones similares: la disminución de las secreciones gástricas.', 1),
+(16, 'Antialérgicos', 'En esta categoría se agrupan fármacos que tienen la finalidad de combatir los efectos negativos de las reacciones alérgicas o la hipersensibilidad.', 1),
+(17, 'Antidiarreicos y laxantes', 'Los antidiarreicos son tipos de medicamentos que tienen como propósito aliviar y detener los efectos de la diarrea.', 1),
+(18, 'Antiinfecciosos', 'Este tipo de medicamentos están recetados para hacer frente a infecciones. Dependiendo del agente infeccioso, se clasifican en antibióticos (contra bacterias), antifúngicos (contra hongos), antivirales (contra virus) y antiparasitarios (contra parásitos).', 1),
+(19, 'Antiinflamatorios', 'Como el propio nombre indica, son fármacos que tienen como finalidad reducir los efecto de la inflamación. Los más recetados son los catalogados como AINES, que además de disminuir la inflamación, tienen efectos analgésicos.', 1),
+(20, 'Antipiréticos', 'Los fármacos antipiréticos son un tipo de medicamentos que tienen la capacidad de reducir la fiebre.', 1),
+(21, 'Antitusivos y mucolíticos', 'Son fármacos que se recetan para tratar de reducir la tos no productiva, es decir, que no libera mucosidad. Se debe tener sumo cuidado con su dosis, ya que algunos de ellos, como la codeína, producen adicción.', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_ingreso`
+--
+
+CREATE TABLE `detalle_ingreso` (
+  `iddetalle_ingreso` int(11) NOT NULL,
+  `idingreso` int(11) NOT NULL,
+  `idarticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_compra` decimal(11,2) NOT NULL,
+  `precio_venta` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `detalle_ingreso`
+--
+
+INSERT INTO `detalle_ingreso` (`iddetalle_ingreso`, `idingreso`, `idarticulo`, `cantidad`, `precio_compra`, `precio_venta`) VALUES
+(12, 8, 6, 59, '67.00', '80.00'),
+(13, 8, 9, 1, '34.00', '87.00'),
+(14, 9, 9, 10, '15.00', '78.00'),
+(15, 9, 7, 11, '15.00', '143.00'),
+(16, 10, 6, 30, '32.00', '40.00');
+
+--
+-- Disparadores `detalle_ingreso`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_updStockIngreso` AFTER INSERT ON `detalle_ingreso` FOR EACH ROW BEGIN
+    UPDATE articulo SET stock =stock + NEW.cantidad
+    WHERE articulo.idarticulo = NEW.idarticulo;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_venta`
+--
+
+CREATE TABLE `detalle_venta` (
+  `iddetalle_venta` int(11) NOT NULL,
+  `idventa` int(11) NOT NULL,
+  `idarticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_venta` decimal(11,2) NOT NULL,
+  `descuento` decimal(11,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `detalle_venta`
+--
+
+INSERT INTO `detalle_venta` (`iddetalle_venta`, `idventa`, `idarticulo`, `cantidad`, `precio_venta`, `descuento`) VALUES
+(6, 5, 6, 2, '80.00', '0.00'),
+(7, 6, 6, 5, '80.00', '0.00'),
+(8, 7, 7, 2, '143.00', '0.00'),
+(9, 8, 7, 4, '143.00', '0.00'),
+(10, 9, 8, 4, '54.00', '0.00'),
+(11, 10, 9, 4, '78.00', '0.00'),
+(12, 11, 10, 12, '50.00', '0.00'),
+(13, 11, 8, 10, '21.00', '0.00');
+
+--
+-- Disparadores `detalle_venta`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_updStockVenta` AFTER INSERT ON `detalle_venta` FOR EACH ROW BEGIN
+    UPDATE
+        articulo
+    SET
+        stock = stock - NEW.cantidad
+    WHERE
+        articulo.idarticulo = NEW.idarticulo ;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ingreso`
+--
+
+CREATE TABLE `ingreso` (
+  `idingreso` int(11) NOT NULL,
+  `idproveedor` int(11) NOT NULL,
+  `idusuario` int(11) NOT NULL,
+  `tipo_comprobante` varchar(20) DEFAULT NULL,
+  `serie_comprobante` varchar(7) NOT NULL,
+  `num_comprobante` varchar(10) NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total_compra` decimal(11,2) NOT NULL,
+  `estado` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `ingreso`
+--
+
+INSERT INTO `ingreso` (`idingreso`, `idproveedor`, `idusuario`, `tipo_comprobante`, `serie_comprobante`, `num_comprobante`, `fecha_hora`, `impuesto`, `total_compra`, `estado`) VALUES
+(8, 11, 1, 'Boleta', '98', '3', '2022-07-23 00:00:00', '0.00', '4017.10', 'Aceptado'),
+(9, 9, 1, 'Boleta', '34', '5', '2022-07-23 00:00:00', '0.00', '315.00', 'Aceptado'),
+(10, 11, 1, 'Ticket', '12', '4', '2022-11-29 00:00:00', '0.00', '960.00', 'Aceptado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permiso`
+--
+
+CREATE TABLE `permiso` (
+  `idpermiso` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `permiso`
+--
+
+INSERT INTO `permiso` (`idpermiso`, `nombre`) VALUES
+(1, 'Escritorio'),
+(2, 'Almacen'),
+(3, 'Compras'),
+(4, 'Ventas'),
+(5, 'Acceso'),
+(6, 'Consulta Compras'),
+(7, 'Consulta Ventas'),
+(8, 'Dashboard');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `idpersona` int(11) NOT NULL,
+  `tipo_persona` varchar(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `tipo_documento` varchar(20) DEFAULT NULL,
+  `num_documento` varchar(20) DEFAULT NULL,
+  `direccion` varchar(70) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`idpersona`, `tipo_persona`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`) VALUES
+(8, 'Proveedor', 'ABBOT LABORATORIOS S.A.', 'RUC', '', 'Av. República de Panamá 3591 piso 7 San isidro – Lima 27', '219-3300', 'Isabel.manrique@abbott.com'),
+(9, 'Proveedor', ' ALCON PHARMACEUTICAL PERU', 'RUC', '', 'Las Begonias 441 piso 3 San Isidro', '2215708', ''),
+(10, 'Proveedor', 'B. BRAUN MEDICAL PERÚ S.A.', 'RUC', '', 'Av. Separadora Industrial No. 887 Urb. Miguel Grau Ate', '326-1825', 'bbraunperu@bbraun.com'),
+(11, 'Proveedor', 'BAYER S.A.', 'RUC', '', 'Av. Paseo de la República No. 3074, Piso 10 San Isidro', '211-3800', 'tatiana.urrea@bayer.com'),
+(12, 'Cliente', 'Publico General', 'DNI', '74575695', 'Las Begonias 441 piso 3 San Isidro', '936173648', 'iscojara14@gmail.com'),
+(13, 'Cliente', 'Federico Alvarez', 'DNI', '74562693', 'Enrique Segoviano', '936174648', 'fico@gmail.com'),
+(14, 'Cliente', 'David Taipe', 'DNI', '99999999', 'Av. República de Panamá 3591 piso 7 San isidro – Lima 27', '219-3300', 'tatiana.urrea@bayer.com'),
+(15, 'Cliente', 'Lucky S.A.C.', 'RUC', '20304269759', 'Jr. Gnral Manuel de Mendiburu Nro. 1230 Int. 102', '219-3300', 'lucky@gmail.com'),
+(16, 'Cliente', 'Leonel Messi', 'CEDULA', '99999999999', 'Rosario, Argentina', '2215708', 'leo_la_pulga@hotmail.com'),
+(17, 'Cliente', 'Marco Angulo', 'DNI', '56568769', 'Unicachi, Villa el Salvador', '972 266 759', 'marco@untels.edu.pe'),
+(18, 'Cliente', 'Sergio Sanchez', 'DNI', '24162693', 'Ovalo Chama, Villa el Salvador', '935 275 325', 'ssanchez@lucky.com.pe'),
+(19, 'Cliente', 'Patrick Mansilla', 'DNI', '70939366', 'Barrio 1, St 2, Mz t, Lt 46, Urb. Pachacamac', '994346173', 'patrick15tk@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `tipo_documento` varchar(20) NOT NULL,
+  `num_documento` varchar(20) NOT NULL,
+  `direccion` varchar(70) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `cargo` varchar(20) DEFAULT NULL,
+  `login` varchar(20) NOT NULL,
+  `clave` varchar(64) NOT NULL,
+  `imagen` varchar(50) NOT NULL,
+  `condicion` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`, `cargo`, `login`, `clave`, `imagen`, `condicion`) VALUES
+(1, 'Team Arqui', 'DNI', '74575695', 'St.7 Gr. 3A Mz. D11 Lt. 46', '936173648', '1813011308@untels.edu.pe', '', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '1655689208.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario_permiso`
+--
+
+CREATE TABLE `usuario_permiso` (
+  `idusuario_permiso` int(11) NOT NULL,
+  `idusuario` int(11) NOT NULL,
+  `idpermiso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuario_permiso`
+--
+
+INSERT INTO `usuario_permiso` (`idusuario_permiso`, `idusuario`, `idpermiso`) VALUES
+(53, 1, 1),
+(54, 1, 2),
+(55, 1, 3),
+(56, 1, 4),
+(57, 1, 5),
+(58, 1, 6),
+(59, 1, 7),
+(60, 1, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `idventa` int(11) NOT NULL,
+  `idcliente` int(11) NOT NULL,
+  `idusuario` int(11) NOT NULL,
+  `tipo_comprobante` varchar(20) NOT NULL,
+  `serie_comprobante` varchar(7) DEFAULT NULL,
+  `num_comprobante` varchar(10) NOT NULL,
+  `fecha_hora` datetime NOT NULL,
+  `impuesto` decimal(4,2) NOT NULL,
+  `total_venta` decimal(11,2) NOT NULL,
+  `estado` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`idventa`, `idcliente`, `idusuario`, `tipo_comprobante`, `serie_comprobante`, `num_comprobante`, `fecha_hora`, `impuesto`, `total_venta`, `estado`) VALUES
+(5, 12, 1, 'Ticket', '98', '3', '2022-07-23 00:00:00', '0.00', '160.00', 'Aceptado'),
+(6, 12, 1, 'Ticket', '98', '5', '2022-11-28 00:00:00', '0.00', '400.00', 'Aceptado'),
+(7, 12, 1, 'Ticket', '23', '23', '2022-11-28 00:00:00', '0.00', '286.00', 'Aceptado'),
+(8, 12, 1, 'Ticket', '98', '4', '2022-11-28 00:00:00', '0.00', '572.00', 'Aceptado'),
+(9, 12, 1, 'Ticket', '98', '34', '2022-11-28 00:00:00', '0.00', '216.00', 'Aceptado'),
+(10, 15, 1, 'Ticket', '12', '1', '2022-11-29 00:00:00', '0.00', '312.00', 'Aceptado'),
+(11, 17, 1, 'Factura', '12', '3', '2022-11-29 00:00:00', '18.00', '810.00', 'Aceptado');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD PRIMARY KEY (`idarticulo`),
+  ADD UNIQUE KEY `nombre_UNIQUE` (`nombre`),
+  ADD KEY `fk_articulo_categoria_idx` (`idcategoria`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idcategoria`),
+  ADD UNIQUE KEY `nombre_UNIQUE` (`nombre`);
+
+--
+-- Indices de la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  ADD PRIMARY KEY (`iddetalle_ingreso`),
+  ADD KEY `fk_detalle_ingreso_ingreso_idx` (`idingreso`),
+  ADD KEY `fk_detalle_ingreso_articulo_idx` (`idarticulo`);
+
+--
+-- Indices de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD PRIMARY KEY (`iddetalle_venta`),
+  ADD KEY `fk_detalle_venta_venta_idx` (`idventa`),
+  ADD KEY `fk_detalle_venta_articulo_idx` (`idarticulo`);
+
+--
+-- Indices de la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  ADD PRIMARY KEY (`idingreso`),
+  ADD KEY `fk_ingreso_persona_idx` (`idproveedor`),
+  ADD KEY `fk_ingreso_usuario_idx` (`idusuario`);
+
+--
+-- Indices de la tabla `permiso`
+--
+ALTER TABLE `permiso`
+  ADD PRIMARY KEY (`idpermiso`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`idpersona`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idusuario`),
+  ADD UNIQUE KEY `login_UNIQUE` (`login`);
+
+--
+-- Indices de la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  ADD PRIMARY KEY (`idusuario_permiso`),
+  ADD KEY `fk_usuario_permiso_usuario_idx` (`idusuario`),
+  ADD KEY `fk_usuario_permiso_permiso_idx` (`idpermiso`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`idventa`),
+  ADD KEY `fk_venta_persona_idx` (`idcliente`),
+  ADD KEY `fk_venta_usuario_idx` (`idusuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  MODIFY `iddetalle_ingreso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  MODIFY `iddetalle_venta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  MODIFY `idingreso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `permiso`
+--
+ALTER TABLE `permiso`
+  MODIFY `idpermiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `idpersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  MODIFY `idusuario_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `idventa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `articulo`
+--
+ALTER TABLE `articulo`
+  ADD CONSTRAINT `fk_articulo_categoria` FOREIGN KEY (`idcategoria`) REFERENCES `categoria` (`idcategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_ingreso`
+--
+ALTER TABLE `detalle_ingreso`
+  ADD CONSTRAINT `fk_detalle_ingreso_articulo` FOREIGN KEY (`idarticulo`) REFERENCES `articulo` (`idarticulo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_ingreso_ingreso` FOREIGN KEY (`idingreso`) REFERENCES `ingreso` (`idingreso`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `detalle_venta`
+--
+ALTER TABLE `detalle_venta`
+  ADD CONSTRAINT `fk_detalle_venta_articulo` FOREIGN KEY (`idarticulo`) REFERENCES `articulo` (`idarticulo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_detalle_venta_venta` FOREIGN KEY (`idventa`) REFERENCES `venta` (`idventa`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `ingreso`
+--
+ALTER TABLE `ingreso`
+  ADD CONSTRAINT `fk_ingreso_persona` FOREIGN KEY (`idproveedor`) REFERENCES `persona` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_ingreso_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `usuario_permiso`
+--
+ALTER TABLE `usuario_permiso`
+  ADD CONSTRAINT `fk_usuario_permiso_permiso` FOREIGN KEY (`idpermiso`) REFERENCES `permiso` (`idpermiso`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_usuario_permiso_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `fk_venta_persona` FOREIGN KEY (`idcliente`) REFERENCES `persona` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_venta_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
